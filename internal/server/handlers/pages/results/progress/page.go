a2a10da1d45eea18e4351817216019412f912aea
Artem Gafarov <a.m.gafarov@ya.ru>
2024-10-25 18:11:54 +0300

    redesign

--- internal/server/handlers/pages/results/progress/page.go
+++ internal/server/handlers/pages/results/progress/page.go
@@ -16,6 +16,7 @@ func New() *ProgressOperaionPage {
 func (n *ProgressOperaionPage) GetPage(c *gin.Context, id string) {
 	c.HTML(http.StatusNotFound, "progress-operation.html", gin.H{
 		"title": "Операция еще выполняется",
-		"style": "/web/styles/results/progress-operation.css",
+		"id":    id,
+		"style": "/web/styles/results/progress-operation-style.css",
 	})
 }
